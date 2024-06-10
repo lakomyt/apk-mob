8fee4586df1cc9170720eaa7a526012065f174d3
lakomyt <tlakomy0@gmail.com>
2024-06-10 20:44:30 +0200

    config template

---
+++ app/config.py
@@ -0,0 +1,8 @@
+# config.py
+
+dbconfig = {
+    'host': 'localhost',     # np. 'localhost' jeśli baza danych jest na lokalnej maszynie
+    'user': 'user',     # nazwa użytkownika do bazy danych
+    'pass': 'kjndfiuwfej', # hasło do bazy danych
+    'name': 'mydb'      # nazwa bazy danych
+}
